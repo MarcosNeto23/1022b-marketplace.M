ebd0e4fba1289db143b60ec57ec8048dc8dfff4f
MarcosNeto23 <marcos.neto@estudante.ifms.edu.br>
2024-09-30 08:24:41 -0400

    atualizações para a hospedagem do servidor

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,11 @@ app.use(cors())
 app.get("/produtos",async (req,res)=>{
     try{
     const conexao = await mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "banco1022b",
-        port: 3306       
+        host: process.env.dbhost?process.env.dbhost: "localhost",
+        user: process.env.dbuser?process.env.dbuser: "root",
+        password: process.env.dbpassword?process.env.dbpassword: "",
+        database: process.env.dbname?process.env.dbname: "banco1022b",
+        port: process.env.dbport?parseInt(process.env.dbport): 3306       
     })
     const [result,fields ] = await conexao.query("SELECT * from produtos")
     res.send(result)
