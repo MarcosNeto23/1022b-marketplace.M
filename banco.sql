8f237047b297ebc539cb9d270557f2d53296e376
MarcosNeto23 <118356000+MarcosNeto23@users.noreply.github.com>
2024-10-21 07:35:53 -0400

    2. Backend

--- banco.sql
+++ banco.sql
@@ -1,4 +1,4 @@
-USE defaultdb;
+USE banco1022b;
 CREATE TABLE IF NOT EXISTS produtos(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
@@ -8,7 +8,15 @@ CREATE TABLE IF NOT EXISTS produtos(
 );
 INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
 
-
+USE banco1022b;
+CREATE TABLE IF NOT EXISTS usuarios(
+id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    created_at timestamp(4),
+    updated_at timestamp(4)
+);
+/*INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');/*
 
 /*
 USE defaultdb;
--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,23 @@ app.get("/produtos",async (req,res)=>{
     }
 })
 
+app.get("/usuarios",async (req,res)=>{
+    try{
+    const conexao = await mysql.createConnection({
+        host: process.env.dbhost?process.env.dbhost: "localhost",
+        user: process.env.dbuser?process.env.dbuser: "root",
+        password: process.env.dbpassword?process.env.dbpassword: "",
+        database: process.env.dbname?process.env.dbname: "banco1022b",
+        port: process.env.dbport?parseInt(process.env.dbport): 3306       
+    })
+    const [result,fields ] = await conexao.query("SELECT * from usuarios")
+    await conexao.end()
+    res.send(result)
+    }catch(e){
+    res.status(500).send("Erro do servidor")
+    }
+})
+
 app.listen(8000,()=>{
     console.log("SERVIDOR INICIADO NA PORTA 8000")
 })
