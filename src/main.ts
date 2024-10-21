6a52a5287a5c84a920dff578da0e83bae3d8e074
MarcosNeto23 <118356000+MarcosNeto23@users.noreply.github.com>
2024-10-21 08:02:15 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ app.get("/produtos",async (req,res)=>{
         host: process.env.dbhost?process.env.dbhost: "localhost",
         user: process.env.dbuser?process.env.dbuser: "root",
         password: process.env.dbpassword?process.env.dbpassword: "",
-        database: process.env.dbname?process.env.dbname: "banco1022b",
+        database: process.env.dbname?process.env.dbname: "defaultdb",
         port: process.env.dbport?parseInt(process.env.dbport): 3306       
     })
     const [result,fields ] = await conexao.query("SELECT * from produtos")
@@ -32,7 +32,7 @@ app.get("/usuarios",async (req,res)=>{
         host: process.env.dbhost?process.env.dbhost: "localhost",
         user: process.env.dbuser?process.env.dbuser: "root",
         password: process.env.dbpassword?process.env.dbpassword: "",
-        database: process.env.dbname?process.env.dbname: "banco1022b",
+        database: process.env.dbname?process.env.dbname: "defaultdb",
         port: process.env.dbport?parseInt(process.env.dbport): 3306       
     })
     const [result,fields ] = await conexao2.query("SELECT * from usuarios")
