abbfdd4eb2a9da95c2d4c882e356b67b5cbc02e3
MarcosNeto23 <118356000+MarcosNeto23@users.noreply.github.com>
2024-10-28 08:07:16 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,3 +1,25 @@
+USE banco1022b;
+CREATE TABLE IF NOT EXISTS produtos(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    descricao VARCHAR(50),
+    preco DECIMAL(10,2),
+    imagem VARCHAR(300)
+);
+INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
+
+USE banco1022b;
+CREATE TABLE IF NOT EXISTS usuarios(
+id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    created_at timestamp(4),
+    updated_at timestamp(4)
+);
+INSERT INTO `banco1022b`.`usuarios` (`id`, `nome`, `email`, `created_at`, `updated_at`) VALUES ('1', 'Marcos', 'marcos.neto@estudante.ifms.edu.br', '2024-10-21 07:40', '2024-10-21 07:41');
+
+
+/*
 USE defaultdb;
 CREATE TABLE IF NOT EXISTS produtos(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
@@ -17,27 +39,4 @@ id BIGINT PRIMARY KEY AUTO_INCREMENT,
     updated_at timestamp(4)
 );
 INSERT INTO `defaultdb`.`usuarios` (`id`, `nome`, `email`, `created_at`, `updated_at`) VALUES ('1', 'Marcos', 'marcos.neto@estudante.ifms.edu.br', '2024-10-21 07:40', '2024-10-21 07:41');
-
-/*
-USE defaultdb;
-CREATE TABLE IF NOT EXISTS produtos(
-	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50),
-    descricao VARCHAR(50),
-    preco DECIMAL(10,2),
-    imagem VARCHAR(300)
-);
-CREATE TABLE IF NOT EXISTS usuarios(
-	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50),
-    idade INT,
-    cpf VARCHAR(50),
-    rg VARCHAR(50),
-    endereco VARCHAR(100),
-    estado_civil ENUM('solteiro','casado')
-);
-INSERT INTO produtos VALUES
-(1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
-
-INSERT INTO usuarios VALUES (1,'João',18,'036.547.382-10','002.874.325','Rua das flores, Bairro dos Planetas, Número 10, Naviraí - MS','casado');
 */
