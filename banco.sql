f03dfbe850db078485a7f350ede3b7fa14a4408a
MarcosNeto23 <118356000+MarcosNeto23@users.noreply.github.com>
2024-12-16 08:25:21 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS produtos(
     preco DECIMAL(10,2),
     imagem VARCHAR(300)
 );
-INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
+INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'https://m.media-amazon.com/images/I/515ahREcK+L._AC_SX679_.jpg');
 
 USE banco1022b;
 CREATE TABLE IF NOT EXISTS usuarios(
